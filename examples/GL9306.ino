d952494f5723a17dbcb05c84aff1eeb7481f97ea
HunterLYZ <83868547+HunterLYZ@users.noreply.github.com>
2022-08-17 21:46:01 +0800

    Update GL9306.ino

--- examples/GL9306.ino
+++ examples/GL9306.ino
@@ -1,15 +1,14 @@
 #define GL9306_SERIAL Serial1
 #include "GL9306.h"
-
+GL9306 optFlow(23, 27);
 void setup()
 {
   Serial.begin(115200);
-  optFlow.begin(23, 27);
 }
 
 void loop()
 {
-  if (optFlow.update())
+  if (optFlow.available())
   {
     optFlow.printValue();
   }
